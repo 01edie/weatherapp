3c7b33b36df217423867e7f4979a585cf51ec7c5
01edie <01bbose@gmail.com>
2022-07-16 00:00:15 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Input from './Input';
 
 function App() {
+    const [weather, setWeather] = useState({});
+    const [image, setImage] = useState('https://images.unsplash.com/photo-1516912481808-3406841bd33c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1344&q=80');
+    const [location, setLocation] = useState('None');
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='heading'>Check Weather</h1>
+      <h2 className="current-location">Current Location : {location}</h2>
+      <Input setWeather={setWeather} setLocation={setLocation} location={location} setImage={setImage} image={image} weather= {weather}></Input>
+
+      <div className="details">
+        <div className="image">
+          <img src={image} alt="location" className='location-image'/>
+        </div>
+        <h4>Current Temperature: {weather?.main?.temp}</h4>
+        <h4>Current Humidity: {weather?.main?.humidity}</h4>
+        <h4>Feels Like: {weather?.main?.feels_like}</h4>
+        <h4>Current Pressure: {weather?.main?.pressure}</h4>
+        <h4>Maximum Temperature: {weather?.main?.temp_max}</h4>
+        <h4>Minimum Temperature: {weather?.main?.temp_min}</h4>
+        <p>[All units are in metric]</p>
+      </div>
+
+
     </div>
+
   );
 }
 
